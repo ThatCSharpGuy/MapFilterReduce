e27bf1075b600b0d84468c35c775edbc6d139987
Antonio Feregrino <antonio.feregrino@gmail.com>
2016-07-16 15:10:36 -0500

    Combinados.

--- MapFilterReduceCSharp/Program.cs
+++ MapFilterReduceCSharp/Program.cs
@@ -6,83 +6,116 @@ using System.Threading.Tasks;
 
 namespace MapFilterReduce
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // Map
-            // El arreglo que queremos transformar
-            var array = new List<int> { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Map
+			// El arreglo que queremos transformar
+			var array = new List<int> { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            //var squaredArray = SquareArray(array); // [4, 9, 16, 25, 36, 49, 64, 81, 100]
+			//var squaredArray = SquareArray(array); // [4, 9, 16, 25, 36, 49, 64, 81, 100]
 
-            var squaredArray = array.Select(x => x * x).ToArray();
+			var squaredArray = array.Select(x => x * x).ToArray();
 
-            // Filter
-            // El arreglo que queremos filtrar
-            var images = new List<string> {
-              "hello.jpg",
-              "world.jpg",
-              "hola.png",
-              "mundo.png",
-              "cats.jpg",
-              "dogs.png"
-            };
+			// Filter
+			// El arreglo que queremos filtrar
+			var images = new List<string> {
+			  "hello.jpg",
+			  "world.jpg",
+			  "hola.png",
+			  "mundo.png",
+			  "cats.jpg",
+			  "dogs.png"
+			};
 
-            //var jpgImages = GetJpgImages(images);
+			//var jpgImages = GetJpgImages(images);
 
-            var jpgImages = images.Where(image => image.EndsWith(".jpg")).ToList(); // ["hello.jpg", "world.jpg", "cats.jpg"]
+			var jpgImages = images.Where(image => image.EndsWith(".jpg")).ToList(); // ["hello.jpg", "world.jpg", "cats.jpg"]
 
-            // Reduce
-            // 
+			// Reduce
+			// 
 
-            //var arraySum = Sum(array); // 54
+			//var arraySum = Sum(array); // 54
 
-            var arraySum = array.Aggregate((acc, x) => acc + x);
+			var arraySum = array.Aggregate((acc, x) => acc + x);
 
-            Console.Read();
-        }
+			// Combinación
+			// Vamos a definir varios ejemplos de ciudades, y meterlos en un arreglo
+			var paris = new City { Name = "Paris", Population = 2243 };
+			var madrid = new City { Name = "Madrid", Population = 3216 };
+			var amsterdam = new City { Name = "Amsterdam", Population = 811 };
+			var berlin = new City { Name = "Berlin", Population = 3397 };
 
-        // Creamos la función que modificará los elementos  
-        // del arreglo elevándolo al cuadrado
-        static List<int> SquareArray(List<int> arr)
-        {
-            var result = new List<int>();
+			var cities = new City[] { paris, madrid, amsterdam, berlin };
 
-            foreach (var x in arr)
-            {
-                result.Add(x * x);
-            }
+			var citiesFilter = cities.Where(city => city.Population > 1000)
+									 .Select(Scale)
+									 .Aggregate("City population", 
+	                                    (result, c) => {
+											return result + $"\n{c.Name}: {c.Population}";});
 
-            return result.ToList();
-        }
+			Console.WriteLine(citiesFilter);
 
-        public static List<string> GetJpgImages(List<string> images)
-        {
-            var results = new List<string>();
+			Console.Read();
+		}
 
-            foreach (var image in images)
-            {
-                if (image.EndsWith(".jpg"))
-                {
-                    results.Add(image);
-                }
-            }
+		// Creamos la función que modificará los elementos  
+		// del arreglo elevándolo al cuadrado
+		static List<int> SquareArray(List<int> arr)
+		{
+			var result = new List<int>();
 
-            return results;
-        }
+			foreach (var x in arr)
+			{
+				result.Add(x * x);
+			}
 
-        public static int Sum(List<int> arr)
-        {
-            var acc = 0;
+			return result.ToList();
+		}
 
-            foreach (var value in arr)
-            {
-                acc += value;
-            }
+		public static List<string> GetJpgImages(List<string> images)
+		{
+			var results = new List<string>();
 
-            return acc;
-        }
+			foreach (var image in images)
+			{
+				if (image.EndsWith(".jpg"))
+				{
+					results.Add(image);
+				}
+			}
 
-    }
+			return results;
+		}
+
+		public static int Sum(List<int> arr)
+		{
+			var acc = 0;
+
+			foreach (var value in arr)
+			{
+				acc += value;
+			}
+
+			return acc;
+		}
+
+
+		// Suponiendo que tenemos la siguiente estructura de ciudades
+		struct City
+		{
+			public string Name;
+			public int Population;
+		}
+
+
+		// Como tenemos las ciudades con poca población, vamos a escribir una función
+		// que nos ayude a escalar la población
+		static City Scale(City city) 
+		{
+			return new City { Name = city.Name, Population = city.Population * 1000 };
+		}
+
+	}
 }
--- MapFilterReduceFSharp/Program.fs
+++ MapFilterReduceFSharp/Program.fs
@@ -1,59 +1,75 @@
 ﻿open System.Collections.Generic
 open System.Linq
 
-let squareArray arr =
-    let result = new List<int>();
+let squareArray arr = 
+    let result = new List<int>()
     for x in arr do
-        result.Add(x*x)
+        result.Add(x * x)
     result
 
-let getJpgImages images : List<string> =
+let getJpgImages images : List<string> = 
     let result = new List<string>()
-    for x:string in images do
-        if (x.EndsWith( ".jpg")) then 
-            result.Add(x)
+    for x : string in images do
+        if (x.EndsWith(".jpg")) then result.Add(x)
     result
 
-let sum (arr : List<int>) : int =
-    let mutable result:int = 0
+let sum (arr : List<int>) : int = 
+    let mutable result : int = 0
     for x in arr do
         result <- result + x
     result
 
+type City = 
+    struct
+        val Name : string
+        val Population : int
+        new(name : string, population : int) = 
+            { Population = population
+              Name = name }
+    end
+
+// Como tenemos las ciudades con poca población, vamos a escribir una función
+// que nos ayude a escalar la población
+let scale(city : City ) : City =
+    new City ( city.Name, city.Population * 1000)
+
 [<EntryPoint>]
 let main argv = 
+
     // Map
     // El arreglo que queremos transformar
-    let array = new List<int>( [| 2; 3; 4; 5; 6; 7; 8; 9; 10|]);
-
+    let array = new List<int>([| 2; 3; 4; 5; 6; 7; 8; 9; 10 |])
     //let squaredArray = squareArray array
+    let squaredArray = array.Select(fun x -> x * x).ToList()
 
-    let squaredArray = array.Select(fun x -> x*x).ToList();
-    
     // Filter
     // El arreglo que queremos filtrar
-    let images = new List<string>([|
-                                    "hello.jpg"
-                                    "world.jpg"
-                                    "hola.png"
-                                    "mundo.png"
-                                    "cats.jpg"
-                                    "dogs.png"
-                                |])
-
+    let images = new List<string>([| "hello.jpg"; "world.jpg"; "hola.png"; "mundo.png"; "cats.jpg"; "dogs.png" |])
     //let jpgImages = getJpgImages images
-
     let jpgImages = images.Where(fun image -> image.EndsWith(".jpg")).ToList()
 
     // Reduce
-    // 
 
     //let arraySum = sum(array); // 54
+    let arraySum = array.Aggregate(fun acc x -> acc + x)
 
-    let arraySum = array.Aggregate( fun acc x -> acc + x)
+
+    // Combinación
+    // Vamos a definir varios ejemplos de ciudades, y meterlos en un arreglo
+    let paris = new City("Paris", 2243)
+    let madrid = new City("Madrid", 3216)
+    let amsterdam = new City("Amsterdam", 811)
+    let berlin = new City("Berlin", 3397)
+
+    let cities = [|paris;madrid;amsterdam;berlin|]
+
+    let citiesFilter = 
+        cities
+        |> Array.filter(fun city -> city.Population > 1000)
+        |> Array.map(scale)
+        |> Array.fold(fun result c -> result + sprintf "\n%s: %d" c.Name c.Population ) "City population"
+
+    System.Console.WriteLine(citiesFilter)
 
     System.Console.ReadKey() |> ignore
-
     0 // devolver un código de salida entero
-
-
